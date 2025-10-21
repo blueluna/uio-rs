80974cec0fcd091fffc66260c7821f74728a7d10
Erik Bånvik <erik.public@gmail.com>
2025-10-22 00:56:36 +0200

    Working on memory mapping issues

--- uio-rs/src/map.rs
+++ uio-rs/src/map.rs
@@ -3,15 +3,18 @@ use crate::device::Device;
 use crate::error::Error;
 use crate::internal::get_page_size;
 use byteorder::{ByteOrder, NativeEndian};
-use memmap2::MmapOptions;
+use memmap2::{MmapOptions, MmapRaw};
+use std::slice;
 
 pub struct Map<'a> {
     /// UIO index
     uio_number: u16,
     /// Mapping index
     map_number: u16,
+    /// Mapping size
+    map_size: usize,
     /// Memory mapping
-    mem_map: memmap2::MmapMut,
+    mem_map: MmapRaw,
     /// UIO device
     _device: &'a Device,
 }
@@ -21,14 +24,18 @@ impl<'a> Map<'a> {
         let description = MapDescription::from_numbers(device.uio(), map_number)?;
 
         let offset = u64::from(description.map_number) * get_page_size();
+        let map_size = description.size;
         let mut options = MmapOptions::new();
-        options.offset(offset).len(description.size);
+        options.offset(offset).len(map_size);
 
-        let mem_map = unsafe { options.map_mut(&device.file) }?;
+        let mem_map = options.map_raw(&device.file)?;
+        // Follwing is required for not getting "double accesses" to registers
+        mem_map.advise(memmap2::Advice::Random)?;
 
         Ok(Self {
             uio_number: description.uio_number,
             map_number: description.map_number,
+            map_size,
             mem_map,
             _device: device,
         })
@@ -42,20 +49,38 @@ impl<'a> Map<'a> {
         self.map_number
     }
 
-    pub fn read_u32(&self, offset: usize) -> Result<u32, Error> {
-        let end = offset + std::mem::size_of::<u32>();
-        if end > self.mem_map.len() {
-            return Err(Error::OutOfBound);
-        }
-        Ok(NativeEndian::read_u32(&self.mem_map[offset..end]))
+    pub fn read_u32(&self, byte_offset: usize) -> Result<u32, Error> {
+        let chunk = self.read_exact(byte_offset, std::mem::size_of::<u32>())?;
+        Ok(NativeEndian::read_u32(&chunk))
     }
 
-    pub fn write_u32(&mut self, offset: usize, value: u32) -> Result<(), Error> {
-        let end = offset + std::mem::size_of::<u32>();
-        if end > self.mem_map.len() {
+    pub fn write_u32(&mut self, byte_offset: usize, value: u32) -> Result<(), Error> {
+        let destination = self.map_slice_mut(byte_offset, std::mem::size_of::<u32>())?;
+        NativeEndian::write_u32(destination, value);
+        Ok(())
+    }
+
+    pub fn read_exact(&self, byte_offset: usize, bytes: usize) -> Result<&[u8], Error> {
+        if (byte_offset+bytes) > self.map_size {
             return Err(Error::OutOfBound);
         }
-        NativeEndian::write_u32(&mut self.mem_map[offset..end], value);
+        Ok(unsafe { slice::from_raw_parts(self.mem_map.as_ptr().wrapping_byte_add(byte_offset), bytes) })
+    }
+
+    pub fn read_all(&self) -> &[u8] {
+        unsafe { slice::from_raw_parts(self.mem_map.as_ptr(), self.map_size) }
+    }
+
+    pub fn write_exact(&self, byte_offset: usize, chunk: &[u8]) -> Result<(), Error> {
+        let destination = self.map_slice_mut(byte_offset, chunk.len())?;
+        destination.copy_from_slice(chunk);
         Ok(())
+    }
+
+    fn map_slice_mut(&self, byte_offset: usize, bytes: usize) -> Result<&mut [u8], Error> {
+        if (byte_offset+bytes) > self.map_size {
+            return Err(Error::OutOfBound);
+        }
+        Ok(unsafe { slice::from_raw_parts_mut(self.mem_map.as_mut_ptr().wrapping_byte_add(byte_offset), bytes) })
     }
 }
